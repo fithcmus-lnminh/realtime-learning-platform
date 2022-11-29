a5ffc5a6ec09bebf6cb19197bcd4a5162b56d152
Le Nhat Minh <lenhatminh11a1@gmail.com>
2022-11-29 15:45:23 +0700

    refactor

--- server/controllers/user.controller.js
+++
@@ -1,9 +0,0 @@
-const { API_CODE_SUCCESS } = require("../constants");
-
-exports.getCurrentUser = (req, res, next) => {
-  return res.json({
-    code: API_CODE_SUCCESS,
-    message: "Success",
-    data: req.user
-  });
-};
--- server/utils/generateToken.js
+++
@@ -1,7 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-const generateToken = (id, secretKey) => {
-  return jwt.sign({ id }, secretKey, { expiresIn: 86400 });
-};
-
-module.exports = generateToken;
