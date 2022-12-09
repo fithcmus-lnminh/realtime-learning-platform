f58143d1037908db0da239ac5f9e5e801a274a2b
Long Huynh <longhuynhpyn01@gmail.com>
2022-12-09 22:12:18 +0700

    render modified date in list presentation

--- src/pages/Presentation/index.jsx
+++ src/pages/Presentation/index.jsx
@@ -14,6 +14,7 @@ import TableCell, { tableCellClasses } from "@mui/material/TableCell";
 import { styled } from "@mui/material/styles";
 import React, { useState } from "react";
 import { BiCommentAdd } from "react-icons/bi";
+import moment from "moment";
 import Alert from "../../components/Alert";
 import "./Presentation.scss";
 
@@ -86,31 +87,93 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
 //   }
 // ];
 
+function betweenDate(start, end) {
+  // console.log("start:", start);
+  // console.log("end:", end);
+
+  const startDate = moment(start);
+  const endDate = moment(end);
+  const diff = endDate.diff(startDate);
+  const diffDuration = moment.duration(diff);
+  const years = diffDuration.years();
+  const months = diffDuration.months();
+  const weeks = diffDuration.weeks();
+  const days = diffDuration.days();
+  const hours = diffDuration.hours();
+  const minutes = diffDuration.minutes();
+  const seconds = diffDuration.seconds();
+  // console.log("Years:", diffDuration.years());
+  // console.log("Months:", diffDuration.months());
+  // console.log("Weeks:", diffDuration.weeks());
+  // console.log("Days:", diffDuration.days());
+  // console.log("Hours:", diffDuration.hours());
+  // console.log("Minutes:", diffDuration.minutes());
+  // console.log("Seconds:", diffDuration.seconds());
+
+  let result = "";
+
+  if (years > 0) result = years > 1 ? `${years} years ago` : "A year ago";
+  else if (months > 0)
+    result = months > 1 ? `${months} months ago` : "A month ago";
+  else if (weeks > 0) result = weeks > 1 ? `${weeks} weeks ago` : "A week ago";
+  else if (days > 0) result = days > 1 ? `${days} days ago` : "A day ago";
+  else if (hours > 0) result = hours > 1 ? `${hours} hours ago` : "A hour ago";
+  else if (minutes > 0)
+    result = minutes > 1 ? `${minutes} minutes ago` : "A minute ago";
+  else result = seconds > 1 ? `${seconds} seconds ago` : "A second ago";
+
+  return result;
+}
+
 const columns = [
   {
     id: "name",
     label: "Name",
-    minWidth: 200
+    minWidth: 180,
+    render: (record) => {
+      return (
+        <Box className="pres__column">{record.name ? record.name : ""}</Box>
+      );
+    }
   },
   {
     id: "owner",
     label: "Owner",
-    minWidth: 100
+    minWidth: 90,
+    render: (record) => {
+      return (
+        <Box className="pres__column">{record.owner ? record.owner : ""}</Box>
+      );
+    }
   },
   {
     id: "modified",
     label: "Modified",
-    minWidth: 100
+    minWidth: 90,
+    render: (record) => {
+      return (
+        <Box className="presentation__column">
+          {record.modified ? betweenDate(record.modified, Date.now()) : ""}
+        </Box>
+      );
+    }
   },
   {
     id: "created",
     label: "Created",
-    minWidth: 100
+    minWidth: 90,
+    render: (record) => {
+      return (
+        <Box className="pres__column">
+          {record.created ? betweenDate(record.created, Date.now()) : ""}
+        </Box>
+      );
+    }
   },
   {
     id: "action",
     label: "Action",
-    minWidth: 200,
+    minWidth: 180,
     align: "right",
     render: (record) => {
       console.log("record:", record);
@@ -130,8 +193,8 @@ const columns = [
   }
 ];
 
-function createData(id, name, owner, modified, created) {
-  return { id, name, modified, owner, created };
+function createData(id, name, owner, created, modified) {
+  return { id, name, owner, created, modified };
 }
 
 const rows = [
@@ -140,35 +203,35 @@ const rows = [
     "Presentation 1",
     "User 1",
     "2022-11-26T06:58:35.493Z",
-    "2022-11-28T17:10:14.763Z"
+    "2022-12-09T10:10:14.763Z"
   ),
   createData(
     "id02",
     "Presentation 2",
     "User 2",
     "2022-11-26T06:58:35.493Z",
-    "2022-11-28T17:10:14.763Z"
+    "2015-12-16T17:10:14.763Z"
   ),
   createData(
     "id03",
     "Presentation 3",
     "User 3",
     "2022-11-26T06:58:35.493Z",
-    "2022-11-28T17:10:14.763Z"
+    "2022-12-09T14:54:29.648Z"
   ),
   createData(
     "id04",
     "Presentation 4",
     "User 4",
     "2022-11-26T06:58:35.493Z",
-    "2022-11-28T17:10:14.763Z"
+    "2022-12-07T17:10:14.763Z"
   ),
   createData(
     "id05",
     "Presentation 5",
     "User 5",
     "2022-11-26T06:58:35.493Z",
-    "2022-11-28T17:10:14.763Z"
+    "2022-12-06T17:10:14.763Z"
   ),
   createData(
     "id06",
@@ -243,6 +306,7 @@ function Presentation() {
         margin: "auto",
         position: "relative"
       }}
+      className="presentation"
     >
       <Alert message={message} onClose={handleCloseAlert} />
 
@@ -292,7 +356,7 @@ function Presentation() {
               </Box>
               <Box>
                 <TableContainer component={Paper}>
-                  <Table sx={{ minWidth: 700 }} aria-label="customized table">
+                  <Table sx={{ minWidth: 630 }} aria-label="customized table">
                     <TableHead>
                       <TableRow>
                         {columns.map((column) => (
@@ -308,7 +372,6 @@ function Presentation() {
                     </TableHead>
                     <TableBody>
                       {rows.map((row) => {
-                        console.log("row:", row);
                         return (
                           <StyledTableRow
                             hover
@@ -317,9 +380,7 @@ function Presentation() {
                             key={row.id}
                           >
                             {columns.map((column) => {
-                              const value = row[column.id];
-                              console.log("column:", column);
-                              console.log("value:", value);
+                              const value = row[column.id] || "";
                               return (
                                 <StyledTableCell
                                   key={column.id}
