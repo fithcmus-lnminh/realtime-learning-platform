820ad5ca27614c12099c152cb7793c981a09e677
Long Huynh <longhuynhpyn01@gmail.com>
2022-12-21 12:58:26 +0700

    customize create presentation

--- src/pages/Presentations/PresentationAddNew/index.jsx
+++ src/pages/Presentations/PresentationAddNew/index.jsx
@@ -2,35 +2,49 @@ import {
   FormHelperText,
   Grid,
   OutlinedInput,
-  DialogContent
+  DialogContent,
+  Select,
+  MenuItem
 } from "@mui/material";
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Controller, useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
+import { isEqual } from "lodash";
 import "./PresentationAddNew.scss";
 import { createPresentation } from "../../../redux/actions/presentationAction";
+import { getAllGroups } from "../../../redux/actions/groupAction";
 import Alert from "../../../components/Alert";
 import Modal from "../../../components/Modal";
 
 const schema = yup
   .object({
-    title: yup.string().required("Please enter presentation title")
+    title: yup.string().required("Please enter presentation title"),
+    type: yup.string().required("Please select type of presentation")
   })
   .required();
 
 function PresentationAddNew(prop) {
   const { open, handleClose, loading, setLoading } = prop;
   const dispatch = useDispatch();
+  const groups = useSelector(
+    (state) => {
+      return state.group.groups;
+    },
+    (prev, next) => isEqual(prev, next)
+  );
   const {
     handleSubmit,
-    formState: { errors, isDirty },
+    formState: { errors },
     control,
-    reset
+    reset,
+    watch,
+    setValue
   } = useForm({
     resolver: yupResolver(schema)
   });
+  const [errorGroup, setErrorGroup] = useState("");
   const [message, setMessage] = useState({
     success: true,
     data: "",
@@ -42,10 +56,28 @@ function PresentationAddNew(prop) {
     reset();
   };
 
+  const typePresentation = watch("type");
+
   const onSubmit = async (data) => {
+    let newData = "";
+    if (data.type === "public") {
+      newData = {
+        title: data.title
+      };
+    } else if (data.type === "private") {
+      if (data.groupId === "") {
+        setErrorGroup("Please select group of presentation");
+        return;
+      }
+      newData = {
+        title: data.title,
+        groupId: data.groupId
+      };
+    }
+
     setLoading(true);
     dispatch(
-      createPresentation(data, handleClose, setLoading, reset, setMessage)
+      createPresentation(newData, handleClose, setLoading, reset, setMessage)
     );
   };
 
@@ -56,6 +88,10 @@ function PresentationAddNew(prop) {
     });
   };
 
+  useEffect(() => {
+    dispatch(getAllGroups("own"));
+  }, [groups]);
+
   return (
     <>
       <Alert message={message} onClose={handleCloseAlert} />
@@ -63,7 +99,6 @@ function PresentationAddNew(prop) {
       <Modal
         title="Create new presentation"
         loading={loading}
-        disableAction={!isDirty}
         actions={["Cancel", "OK"]}
         actionText="Create"
         show={open}
@@ -74,6 +109,7 @@ function PresentationAddNew(prop) {
           <Grid>
             <Controller
               name="title"
+              defaultValue=""
               control={control}
               render={({ field }) => {
                 return (
@@ -101,6 +137,97 @@ function PresentationAddNew(prop) {
                 );
               }}
             />
+            <Controller
+              name="type"
+              defaultValue=""
+              control={control}
+              render={({ field }) => {
+                return (
+                  <Grid item xs={12}>
+                    <p className="required form__label">Type</p>
+                    <Select
+                      id="type"
+                      sx={{ width: 500, mb: 1, mt: 1 }}
+                      fullWidth
+                      error={!!errors.type?.message}
+                      /* eslint-disable react/jsx-props-no-spreading */
+                      {...field}
+                      onChange={(e) => {
+                        if (e.target.value === "public") {
+                          setValue("groupId", "");
+                          setErrorGroup("");
+                        }
+                        field.onChange(e);
+                      }}
+                    >
+                      <MenuItem value="private">Private</MenuItem>
+                      <MenuItem value="public">Public</MenuItem>
+                    </Select>
+                    {errors.type?.message && (
+                      <FormHelperText
+                        sx={{ mb: 2, mt: 0 }}
+                        id="component-error-text"
+                        error
+                      >
+                        {errors.type.message}
+                      </FormHelperText>
+                    )}
+                  </Grid>
+                );
+              }}
+            />
+            {typePresentation === "private" && (
+              <Controller
+                name="groupId"
+                defaultValue=""
+                control={control}
+                render={({ field }) => {
+                  return (
+                    <Grid item xs={12}>
+                      <p className="required form__label">Group</p>
+                      <Select
+                        id="groupId"
+                        sx={{ width: 500, mb: 1, mt: 1 }}
+                        fullWidth
+                        error={!!errorGroup}
+                        /* eslint-disable react/jsx-props-no-spreading */
+                        {...field}
+                        onChange={(e) => {
+                          if (e.target.value === "") {
+                            setErrorGroup(
+                              "Please select group of presentation"
+                            );
+                          } else {
+                            setErrorGroup("");
+                          }
+                          field.onChange(e);
+                        }}
+                      >
+                        {groups.length > 0
+                          ? groups.map((group) => (
+                              <MenuItem
+                                value={group?.groupId.id}
+                                key={group?.groupId?.id}
+                              >
+                                {group?.groupId?.name}
+                              </MenuItem>
+                            ))
+                          : null}
+                      </Select>
+                      {errorGroup && (
+                        <FormHelperText
+                          sx={{ mb: 2, mt: 0 }}
+                          id="component-error-text"
+                          error
+                        >
+                          {errorGroup}
+                        </FormHelperText>
+                      )}
+                    </Grid>
+                  );
+                }}
+              />
+            )}
           </Grid>
         </DialogContent>
       </Modal>
